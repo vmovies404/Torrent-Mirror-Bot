81aa0c5d6a463310a2b988eddf7406c06a122a12
vmovies404 <84915625+vmovies404@users.noreply.github.com>
2021-05-30 15:05:41 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -83,7 +83,7 @@ def bot_help(update, context):
 
 /{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
 
-Watch video - https://youtu.be/07Pj9YLdpM4
+Watch video - https://youtu.be/Fpbjsnj2h_U
 '''
     sendMessage(help_string, context.bot, update)
 
